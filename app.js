28836e16c28c3e7b49c59bada5e67b5cef81081d
TomTimmel <tom.t.timmel@gmail.com>
2016-08-16 13:31:34 -0700

    update files for safety

---
+++ app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var userName = prompt('Hello, what\'s your name?');
+alert('Thanks ' + userName + ', please answer the following questions for me by typing yes or no.  OK?');
+console.log ('The users name is ' + userName);
+
+var whereAmIFrom = prompt('Am I from Alaska?');
+
+if(whereAmIFrom.toLowerCase() === 'yes' || whereAmIFrom.toLowerCase() === 'y') {
+  alert('Right! I lived in Alaska for the last 20 years, but I grew up in Cincinnati...');
+  console.log('There answer was correct, I am from Alaska');
+  } else if(whereAmIFrom.toLowerCase() === 'no' || whereAmIFrom.toLowerCase() === 'n'){
+  alert('Actually I am from Alaska');
+  console.log('There answer was incorrect, I am from Alaska');
+  } else {
+  alert('Are you really not able to follow instructions, answer yes, or no!');
+  console.log('They are too dumb to answer yes or no');
+  }
+
+var myFavoriteGame = prompt('Do I love to play Ultimate Frisbee?');
+
+if(myFavoriteGame.toLowerCase() === 'yes' || myFavoriteGame.toLowerCase() === 'y') {
+  alert('Yes! I love to play Ultimate, my league team this year was a lot of fun. I also like basketball...');
+  console.log('There answer was correct, I love ultimate');
+  } else if(myFavoriteGame.toLowerCase() === 'no' || myFavoriteGame.toLowerCase() === 'n'){
+  alert('Nope, Ultimate is my favorite game');
+  console.log('There answer was incorrect, I love ultimate')
+  } else {
+  alert('Are you really not able to follow instructions, answer yes, or no!');
+  console.log('They are too dumb to answer yes or no');
+  }
+
+  var girlfriend = prompt('Do I have a girlfriend named Christine?');
+
+  if(girlfriend.toLowerCase() === 'yes' || girlfriend.toLowerCase() === 'y') {
+    alert('Yes! I\'m pretty lucky, she\'s great!');
+    console.log('There answer was correct, Christine is my girlfriend');
+    } else if(girlfriend.toLowerCase() === 'no' || girlfriend.toLowerCase() === 'n'){
+    alert('Nope, I do have a girlfriend named Christine');
+    console.log('There answer was incorrect, I have a girlfriend named Christine')
+    } else {
+    alert('Are you really not able to follow instructions, answer yes, or no!');
+    console.log('They are too dumb to answer yes or no');
+    }
